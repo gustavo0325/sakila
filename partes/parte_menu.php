4e72d620b3e55945efa21764ffdabbea7037a966
gustavo <gustavocepeda9903@gmail.com>
2020-06-25 21:48:04 -0400

    archivo con menu y header de sakila

---
+++ partes/parte_menu.php
@@ -0,0 +1,53 @@
+<nav>
+<ul class="nav justify-content-end bg-dark text-black-50 mb-2 ">
+
+    <li class="nav-item">
+        <a class="nav-link text-light" aria-current="page" href="index.php"><i class="fas fa-home mr-1"></i>Inicio</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" aria-current="page" href="actor.php"><i class="fas fa-user mr-2"></i>Actor</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="direccion.php"><i class="fas fa-map-marker-alt"></i> Direccion</a>
+    </li>
+
+    <li class="nav-item ">
+        <a class="nav-link text-light" href="categoria.php"><i class="fas fa-tag mr-1"></i>Categoria</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="ciudad.php"><i class="fas fa-city mr-2"></i>Ciudad</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="pais.php"><i class="fas fa-flag mr-2"></i>Pais</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="cliente.php"><i class="fas fa-user-tag mr-1"></i>Cliente</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="pelicula.php"><i class="fas fa-film mr-1"></i>Pelicula</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="idioma.php"><i class="fa fa-language mr-1" aria-hidden="true"></i>Idioma</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="personal.php"><i class="fa fa-users mr-1"></i>Personal</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-light" href="tienda.php"><i class="fas fa-shopping-bag mr-1"></i>Tienda</a>
+    </li>
+
+</ul>
+</nav>
+
+<h1 class="text-light container"> <?php echo $nombrepagina; ?> </h1>
+
+
